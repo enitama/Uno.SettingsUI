e6bb47ffa93555b29bdd6e948d25db1b8d25ec8d
Mahdi Hosseini <mahdidvb72@gmail.com>
2022-06-04 09:35:59 +0430

    Add OSVersionHelper and Update to WASDK 1.1.0 stable

--- src/SettingsUI/Tools/Helpers/NativeMethods.cs
+++ src/SettingsUI/Tools/Helpers/NativeMethods.cs
@@ -5,6 +5,32 @@ internal static class NativeMethods
 {
     internal delegate IntPtr WinProc(IntPtr hWnd, WindowMessage Msg, IntPtr wParam, IntPtr lParam);
 
+    [DllImport("NTdll.dll")]
+    internal static extern int RtlGetVersion(out RTL_OSVERSIONINFOEX lpVersionInformation);
+
+    [StructLayout(LayoutKind.Sequential)]
+    internal struct RTL_OSVERSIONINFOEX
+    {
+        public RTL_OSVERSIONINFOEX(uint dwMajorVersion, uint dwMinorVersion, uint dwBuildNumber, uint dwRevision, uint dwPlatformId, string szCSDVersion) : this()
+        {
+            this.dwMajorVersion = dwMajorVersion;
+            this.dwMinorVersion = dwMinorVersion;
+            this.dwBuildNumber = dwBuildNumber;
+            this.dwRevision = dwRevision;
+            this.dwPlatformId = dwPlatformId;
+            this.szCSDVersion = szCSDVersion;
+        }
+        public readonly uint dwOSVersionInfoSize { get; init; } = (uint) Marshal.SizeOf<RTL_OSVERSIONINFOEX>();
+
+        public uint dwMajorVersion;
+        public uint dwMinorVersion;
+        public uint dwBuildNumber;
+        public uint dwRevision;
+        public uint dwPlatformId;
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 128)]
+        public string szCSDVersion;
+    }
+
     /// <summary>
     /// Places the window at the top of the Z order.
     /// </summary>
