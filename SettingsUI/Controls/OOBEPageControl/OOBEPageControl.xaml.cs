f4b283e2528e18e222f9dd80c713dd881008f29b
Mahdi Hosseini <mahdidvb72@gmail.com>
2023-02-16 14:03:49 +0330

    Update OOBE Page

--- SettingsUI/Controls/OOBEPageControl/OOBEPageControl.xaml.cs
+++ SettingsUI/Controls/OOBEPageControl/OOBEPageControl.xaml.cs
@@ -36,6 +36,11 @@ public sealed partial class OOBEPageControl : UserControl
         set => SetValue(HeroImageProperty, value);
     }
 
+    public double HeroImageHeight
+    {
+        get { return (double) GetValue(HeroImageHeightProperty); }
+        set { SetValue(HeroImageHeightProperty, value); }
+    }
     public object PageContent
     {
         get { return (object) GetValue(PageContentProperty); }
@@ -46,4 +51,5 @@ public sealed partial class OOBEPageControl : UserControl
     public static readonly DependencyProperty DescriptionProperty = DependencyProperty.Register("Description", typeof(string), typeof(SettingsPageControl), new PropertyMetadata(default(string)));
     public static readonly DependencyProperty HeroImageProperty = DependencyProperty.Register("HeroImage", typeof(string), typeof(SettingsPageControl), new PropertyMetadata(default(string)));
     public static readonly DependencyProperty PageContentProperty = DependencyProperty.Register("PageContent", typeof(object), typeof(SettingsPageControl), new PropertyMetadata(new Grid()));
+    public static readonly DependencyProperty HeroImageHeightProperty = DependencyProperty.Register("HeroImageHeight", typeof(double), typeof(SettingsPageControl), new PropertyMetadata(280.0));
 }
